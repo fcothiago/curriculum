894b731e057106a4b05fe4d72b136bfe77f2b994
fcothiago <f.t.s.goncalves@gmail.com>
2025-07-19 16:37:37 -0300

    finished bg transition script

--- js/navigator.js
+++ js/navigator.js
@@ -1,11 +1,10 @@
 //https://coolors.co/palette/05668d-028090-00a896-02c39a-f0f3bd
+import {updatePattern , addPattern} from "./bganimation.js"
 let covertoggler = 0;
-const bgpatterns = {
-	intro:'linear-gradient(45deg,rgba(5, 102, 141, 1) 50%, rgba(42, 144, 184, 1) 70%, rgba(5, 102, 141, 1) 90%)',
-	resume:'linear-gradient(45deg,rgba(2, 128, 144, 1) 40%, rgba(31, 166, 184, 1) 60%, rgba(2, 128, 144, 1) 80%)',
-	professional:'linear-gradient(45deg,rgba(2, 195, 154, 1) 30%, rgba(67, 230, 194, 1) 50%, rgba(2, 195, 154, 1) 70%)',
-	portifolio:'linear-gradient(45deg,rgba(2, 195, 154, 1) 20%, rgba(48, 227, 188, 1) 40%, rgba(2, 195, 154, 1) 60%)'
-};
+addPattern('intro','linear-gradient(45deg,rgba(5, 102, 141, 1) 50%, rgba(42, 144, 184, 1) 70%, rgba(5, 102, 141, 1) 90%)');
+addPattern('resume','linear-gradient(45deg,rgba(2, 128, 144, 1) 40%, rgba(31, 166, 184, 1) 60%, rgba(2, 128, 144, 1) 80%)');
+addPattern('professional','linear-gradient(45deg,rgba(2, 195, 154, 1) 30%, rgba(67, 230, 194, 1) 50%, rgba(2, 195, 154, 1) 70%)');
+addPattern('portifolio','linear-gradient(45deg,rgba(2, 195, 154, 1) 20%, rgba(48, 227, 188, 1) 40%, rgba(2, 195, 154, 1) 60%)');
 const options = [
 	{value:'intro',text:'Intro'},
 	{value:'resume',text:'Formação e Certificados'},
@@ -13,13 +12,6 @@ const options = [
 	{value:'portifolio',text:'Portifólio'}
 ];
 const secselector = document.querySelector('#secselector');
-const updadetebgpattern = (pattern) => {
-	const cover = [document.querySelector('div#cover1'),document.querySelector('div#cover2')];
-	cover[1-covertoggler].style.background = bgpatterns[pattern];
-	cover[covertoggler].style.opacity = '0';
-	cover[1-covertoggler].style.opacity = '1';
-	covertoggler = 1-covertoggler;
-};
 options.forEach( (item) => {
 	const option = document.createElement('option');
 	option.value = item.value;
@@ -27,11 +19,7 @@ options.forEach( (item) => {
 	secselector.appendChild(option);
 });
 secselector.addEventListener('change', (e) =>{
-	pattern  = e.target.value;
-	updadetebgpattern(pattern);	
+	const pattern  = e.target.value;
+	updatePattern(pattern);
 });
-updadetebgpattern();
-document.querySelector('div#cover1').style.transition = 'opacity 1s ease-in-out';
-document.querySelector('div#cover2').style.transition = 'opacity 1s ease-in-out';
-
-
+updatePattern("intro");

660e090b16c9aa6d1a84d6d771feded886f83213
fcothiago <f.t.s.goncalves@gmail.com>
2025-07-20 18:27:54 -0300

    picture animation done

--- js/navigator.js
+++ js/navigator.js
@@ -1,16 +1,15 @@
 //https://coolors.co/palette/05668d-028090-00a896-02c39a-f0f3bd
 import {updatePattern , addPattern} from "./bganimation.js";
 import {updatePosition} from "./picanimation.js"
-let covertoggler = 0;
 addPattern('intro','linear-gradient(45deg,rgba(5, 102, 141, 1) 50%, rgba(42, 144, 184, 1) 70%, rgba(5, 102, 141, 1) 90%)');
 addPattern('resume','linear-gradient(45deg,rgba(2, 128, 144, 1) 40%, rgba(31, 166, 184, 1) 60%, rgba(2, 128, 144, 1) 80%)');
 addPattern('professional','linear-gradient(45deg,rgba(2, 195, 154, 1) 30%, rgba(67, 230, 194, 1) 50%, rgba(2, 195, 154, 1) 70%)');
 addPattern('portifolio','linear-gradient(45deg,rgba(2, 195, 154, 1) 20%, rgba(48, 227, 188, 1) 40%, rgba(2, 195, 154, 1) 60%)');
 const options = [
-	{value:'intro',text:'Intro'},
-	{value:'resume',text:'Formação e Certificados'},
-	{value:'professional',text:'Experiência'},
-	{value:'portifolio',text:'Portifólio'}
+	{value:'intro',text:'Intro',bg:'url("../pictures/profile.jpeg")'},
+	{value:'resume',text:'Formação e Certificados',bg:'url("../pictures/education.png")'},
+	{value:'professional',text:'Experiência',bg:'url("../pictures/professional.png")'},
+	{value:'portifolio',text:'Portifólio',bg:'url("../pictures/portifolio.png")'}
 ];
 const secselector = document.querySelector('#secselector');
 options.forEach( (item) => {
@@ -21,8 +20,10 @@ options.forEach( (item) => {
 });
 secselector.addEventListener('change', (e) =>{
 	const pattern  = e.target.value;
-	const position = options.findIndex( item => item.value = e.target.value );
+	const position = options.findIndex( (item) => item.value == e.target.value );
 	updatePattern(pattern);
-	updatePosition(position,options.length);
+	updatePosition(position,options.length-1,options[position].bg);
 });
 updatePattern("intro");
+updatePosition(0,options.length-1,options[0].bg);
+
--- js/picanimation.js
+++ js/picanimation.js
@@ -1,18 +1,33 @@
+let context = {current:0,items:0};
 const picture = document.querySelector('div#picture');
 const picturearea = document.querySelector('section#picture-area');
 const main = document.querySelector('main');
-const updatePosition = (current,items) => {
+const updatePosition = (current,items,bg) => {
+	context.current = current;
+	context.items = items;
 	const flexdirection = window.getComputedStyle(main)['flex-direction'];
 	if(flexdirection == 'column')
 	{
 		const totalsize = picturearea.getBoundingClientRect().width;
 		const picturesize = picture.getBoundingClientRect().width;
 		const margin = (totalsize-picturesize)*(current/items);
-		picturesize.style['margin-left'] = margin;
+		picture.style['margin'] = `auto 0 auto ${margin}px`;
 	}
 	else
 	{
+		const totalsize = picturearea.getBoundingClientRect().height;
+		const picturesize = picture.getBoundingClientRect().height;
+		const margin = (totalsize-picturesize)*(current/items);
+		picture.style['margin'] = `${margin}px 5px 0 0`;
+	}
+	if(bg)
+	{
+		picture.style['background'] = bg;
+		picture.style['background-size'] = 'cover';
 
 	}
 };
+window.addEventListener('resize', () => {
+	updatePosition (context.current,context.items); 
+});
 export{ updatePosition };

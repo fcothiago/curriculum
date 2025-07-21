f0db4c526ed3199b255e341172684921f4085e56
fcothiago <f.t.s.goncalves@gmail.com>
2025-07-21 19:24:11 -0300

    improved backgrounds and layout

--- js/bganimation.js
+++ js/bganimation.js
@@ -2,8 +2,8 @@ let covertoggler = 0;
 let bgpatterns = {};
 const cover = [document.querySelector('div#cover1'),document.querySelector('div#cover2')];
 const updatePattern = (pattern) => {
-	document.querySelector('body').style.background = bgpatterns[pattern];
-        cover[1-covertoggler].style.background = bgpatterns[pattern];
+	document.querySelector('body').style['background-image'] = bgpatterns[pattern];
+        cover[1-covertoggler].style['background-image']  = bgpatterns[pattern];
         cover[covertoggler].style.opacity = '0';
         cover[1-covertoggler].style.opacity = '1';
         covertoggler = 1-covertoggler;
--- js/navigator.js
+++ js/navigator.js
@@ -1,15 +1,15 @@
 //https://coolors.co/palette/05668d-028090-00a896-02c39a-f0f3bd
 import {updatePattern , addPattern} from "./bganimation.js";
 import {updatePosition} from "./picanimation.js"
-addPattern('intro','linear-gradient(45deg,rgba(5, 102, 141, 1) 50%, rgba(42, 144, 184, 1) 70%, rgba(5, 102, 141, 1) 90%)');
-addPattern('resume','linear-gradient(45deg,rgba(2, 128, 144, 1) 40%, rgba(31, 166, 184, 1) 60%, rgba(2, 128, 144, 1) 80%)');
+addPattern('intro',"url('../pictures/backgrounds/introbg.jpg')");
+addPattern('resume',"url('../pictures/backgrounds/educationbg.png')");
 addPattern('professional','linear-gradient(45deg,rgba(2, 195, 154, 1) 30%, rgba(67, 230, 194, 1) 50%, rgba(2, 195, 154, 1) 70%)');
 addPattern('portifolio','linear-gradient(45deg,rgba(2, 195, 154, 1) 20%, rgba(48, 227, 188, 1) 40%, rgba(2, 195, 154, 1) 60%)');
 const options = [
-	{value:'intro',text:'Intro',bg:'url("./pictures/profile.jpeg")'},
-	{value:'resume',text:'Formação e Certificados',bg:'url("./pictures/education.png")'},
-	{value:'professional',text:'Experiência',bg:'url("./pictures/professional.png")'},
-	{value:'portifolio',text:'Portifólio',bg:'url("./pictures/portifolio.png")'}
+	{value:'intro',text:'Intro',bg:'url("../pictures/profile.jpeg")'},
+	{value:'resume',text:'Formação e Certificados',bg:'url("../pictures/education.png")'},
+	{value:'professional',text:'Experiência',bg:'url("../pictures/professional.png")'},
+	{value:'portifolio',text:'Portifólio',bg:'url("../pictures/portifolio.png")'}
 ];
 const secselector = document.querySelector('#secselector');
 const prevbutton = document.querySelector('button#prevbtn');
@@ -44,7 +44,6 @@ prevbutton.addEventListener('click',(e) => {
 	updatePosition(position,options.length-1,options[position].bg);
 	secselector.value = options[position].value;
 });
-
 updatePattern("intro");
 updatePosition(0,options.length-1,options[0].bg);
 

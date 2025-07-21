c20d48b02e32ee0285158b6344f0076ea7f41f8c
fcothiago <f.t.s.goncalves@gmail.com>
2025-07-21 14:47:47 -0300

    prev and next buttons implementation started

--- js/navigator.js
+++ js/navigator.js
@@ -12,18 +12,39 @@ const options = [
 	{value:'portifolio',text:'Portifólio',bg:'url("../pictures/portifolio.png")'}
 ];
 const secselector = document.querySelector('#secselector');
+const prevbutton = document.querySelector('button#prevbtn');
+const nextbutton = document.querySelector('button#nextbtn');
 options.forEach( (item) => {
 	const option = document.createElement('option');
 	option.value = item.value;
 	option.text = item.text;
 	secselector.appendChild(option);
 });
-secselector.addEventListener('change', (e) =>{
+secselector.addEventListener('change', (e) => {
 	const pattern  = e.target.value;
 	const position = options.findIndex( (item) => item.value == e.target.value );
 	updatePattern(pattern);
 	updatePosition(position,options.length-1,options[position].bg);
 });
+nextbutton.addEventListener('click',(e) => {
+	//e.preventDefault();
+	const current = secselector.value;
+	let position = options.findIndex( (item) => item.value == current );
+	position = position < options.length ? position + 1 : position;
+	updatePattern(options[position].value);
+	updatePosition(position,options.length-1,options[position].bg);
+	secselector.value = options[position].value;
+});
+prevbutton.addEventListener('click',(e) => {
+	//e.pŕeventDefault();
+	const current = secselector.value;
+	let position = options.findIndex( (item) => item.value == current );
+	position = position > 0 ? position - 1 : position;
+	updatePattern(options[position].value);
+	updatePosition(position,options.length-1,options[position].bg);
+	secselector.value = options[position].value;
+});
+
 updatePattern("intro");
 updatePosition(0,options.length-1,options[0].bg);
 
